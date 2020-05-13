e8e2f715b9a60f50836bd834fd10bdff17adc3d7
riverandeye <riverandeye@gmail.com>
2020-05-13 16:41:56 +0900

    Updates

---
+++ precache-manifest.d5cc69de2b10d773848e8cc9b9ee79a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0beaa7713f7409b5d55f61b92bfd29ab",
+    "url": "/gwantyle/index.html"
+  },
+  {
+    "revision": "b31cee387fe988b8ee73",
+    "url": "/gwantyle/static/js/2.05b11fd8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gwantyle/static/js/2.05b11fd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90fcc0d886a919f2cf09",
+    "url": "/gwantyle/static/js/main.60b4428f.chunk.js"
+  },
+  {
+    "revision": "d3ce906f1075b25ee5bc",
+    "url": "/gwantyle/static/js/runtime-main.6b096a02.js"
+  }
+]);
